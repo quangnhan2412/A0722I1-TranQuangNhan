725cb506a40d3898255431acdf5cb59c2422408b
Your Name <you@example.com>
2022-11-19 14:39:39 +0700

    ten

---
+++ module02/src/ss13_search_algorithm/dequy/BinarySearchWithRecusion.java
@@ -0,0 +1,30 @@
+package ss13_search_algorithm.dequy;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BinarySearchWithRecusion {
+    static int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+
+    public static int binarySearch(int[] array, int left, int right, int value) {
+        if (left <= right) {
+            int middle = (left + right) / 2;
+            if (value == array[middle]) {
+                return middle;
+            } else if (value < array[middle]) {
+                return binarySearch(array, left, middle - 1, value);
+            } else
+                return binarySearch(array, middle + 1, right, value);
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(binarySearch(list, 0, list.length - 1, 2));
+        System.out.println(binarySearch(list, 0, list.length - 1, 11));
+        System.out.println(binarySearch(list, 0, list.length - 1, 79));
+        System.out.println(binarySearch(list, 0, list.length - 1, 1));
+        System.out.println(binarySearch(list, 0, list.length - 1, 5));
+        System.out.println(binarySearch(list, 0, list.length - 1, 80));
+    }
+}
